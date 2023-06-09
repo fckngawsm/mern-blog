4666b3d394431e4aa473a20c46799184d4c6ea43
Kirill <forworkskirill@gmail.com>
2023-06-09 22:21:46 +0500

    init jwt and using express.json

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const app = express();
+// позволяет читать json из наших запросов
+app.use(express.json());
 
 const { PORT = 4444 } = process.env;
 
 app.get("/", (req, res) => {
   res.send("hello worsdasdassssld");
+});
+app.post("/auth/login", (req, res) => {
+  const { email, password } = req.body;
+  const token = jwt.sign(
+    {
+      email,
+    },
+    "secret-key"
+  );
+  res.json({ succes: true, token });
 });
 
 app.listen(PORT, () => {
